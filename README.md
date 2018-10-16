24c46cc31f5ed38a8c004e8f64cd873580eb3cdf
Dawn M. Foster <dawn@dawnfoster.com>
2018-10-16 11:47:31 +0100

    added the README and the script I use to sync local forked repos back into upstream

---
+++ README.md
@@ -0,0 +1,9 @@
+Repository where I keep the scripts that I've written for various purposes.
+
+Some of these are a bit hacky with hard-coded info and may not be really useful for anyone else :)
+
+## License and Copyright
+
+Licensed under GNU General Public License (GPL), version 3 or later.
+
+Copyright (C) 2018 Dawn M. Foster
---
+++ sync_fork.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Copyright (C) 2018 Dawn M. Foster
+# Licensed under GNU General Public License (GPL), version 3 or later: http://www.gnu.org/licenses/gpl.txt
+ 
+# Quick Script to sync a fork from Github with the latest
+# changes from the upstream project. This is mostly so I stop
+# forgetting that last step of pushing the changes.
+
+git fetch upstream
+git checkout master
+git merge upstream/master
+git push origin master
