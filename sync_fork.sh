5a0afd5f477319b213732b470d4be4005de0b66b
Dawn M. Foster <dafoster@pivotal.io>
2018-11-16 17:10:25 +0000

    added a comment about setting upstream before running this script

--- sync_fork.sh
+++ sync_fork.sh
@@ -2,6 +2,9 @@
 
 # Copyright (C) 2018 Dawn M. Foster
 # Licensed under GNU General Public License (GPL), version 3 or later: http://www.gnu.org/licenses/gpl.txt
+
+# Need to set upstream first using something like:
+# git remote add upstream https://github.com/ORIGINAL_OWNER/ORIGINAL_REPOSITORY.git
  
 # Quick Script to sync a fork from Github with the latest
 # changes from the upstream project. This is mostly to prevent me from
