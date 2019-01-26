55e4f2a27e9f4aa06f7f9db4747b2c5e672169e0
Dawn M. Foster <dawn@dawnfoster.com>
2019-01-26 10:14:09 +0000

    made more robust with command line optiions and option to backup with or without directory versioning based on date

--- quick_file_backup.sh
+++ quick_file_backup.sh
@@ -1,7 +1,51 @@
 #!/bin/bash
 
-DATE=`date +%Y-%m-%d-%H-%M`'_FOSDEM_2019'
-echo $DATE
+# Copyright (C) 2015 Dawn M. Foster
+# Licensed under GNU General Public License (GPL), version 3 or later: http://www.gnu.org/licenses/gpl.txt
 
-cp -R /Users/dafoster/presentations/FOSDEM_2019/ /Users/dafoster/Dropbox/Backups/$DATE
+# Examples
+# ./quick_file_backup.sh --source_path /Volumes/GoogleDrive/My\ Drive/Presentations_and_Events/ --dest_path ~/Documents/file_backup/
+# ./quick_file_backup.sh --source_path /Volumes/GoogleDrive/My\ Drive/Presentations_and_Events/FOSDEM_2019/ --dest_path ~/Dropbox/Backups/ --versioning
+
+# --versioning		Use if you want the files dropped into timestamped dirs (leaves a bajillion dirs to clean up)
+# --source_path 	Path where the files exist now - include trailing slash
+# --dest_path		Path where the copied files will live - include trailing slash
+
+# Set Defaults
+VERSIONING=0
+DATE=`date +%Y-%m-%d-%H-%M-%S`
+
+# Read arguments from the command line and store them in variables
+
+while [[ $# > 0 ]]
+do
+key="$1"
+
+case $key in
+     --versioning)
+     VERSIONING=1
+     shift
+     ;;
+     --source_path)
+     SOURCE_PATH="$2"
+     shift
+     ;;
+     --dest_path)
+     DEST_PATH="$2"
+     shift
+     ;;
+        *)
+            # unknown option
+     ;;
+esac
+
+shift
+done
+
+if [ $VERSIONING = 1 ]; then
+    cp -R "$SOURCE_PATH" "$DEST_PATH"$DATE
+else
+    cp -R "$SOURCE_PATH" "$DEST_PATH"
+fi
+
 
